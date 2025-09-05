fef8cff0724623f9cc195b8790826b846453532c
harrisontolley <htolley0@gmail.com>
2025-09-06 09:54:18 +1000

    Unused files.

--- src/eval/stub_eval.cc
+++
@@ -1,8 +0,0 @@
-#include "eval.hh"
-#include "../engine/board.hh"
-
-namespace eval
-{
-    int evaluate(const engine::Board &) { return 0; }
-    bool load_weights(const char *) { return true; }
-}
